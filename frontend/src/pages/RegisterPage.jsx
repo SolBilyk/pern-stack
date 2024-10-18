7d0c4cde99f0548bb94603ef4311df98734d42a7
Sol Bilyk <solbilyk14@gmail.com>
2024-10-18 00:22:52 -0300

    Frontend-registro terminado, clase 9 parte 5 completa

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+
+import {Button, Card, Input} from "../components/ui"
+import {useForm} from "react-hook-form"; //permite crear en onstage
 
 function RegisterPage() {
+
+  const {register, handleSubmit, formState: {errors}} = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
-    <div>RegisterPage</div>
-  )
+    <div  className="h-[calc(100vh-64px)] flex items-center justify-center">
+      
+      <Card>
+      <h3 className='text-2xl font-bold' >Registro</h3>
+        <form onSubmit={onSubmit}> 
+          <Input placeholder=" Ingrese su nombre "
+          {... register("name", {required:true})}></Input>
+
+          {
+            errors.name && <p className="text-red-500"> Este campo es requerido </p>
+          }
+
+          <Input type="email" placeholder=" Ingrese su email " 
+          {... register("email", {required:true})}></Input>
+
+          {
+            errors.email && <p className="text-red-500"> Este campo es requerido </p>
+          }
+
+
+          <Input type="password" placeholder=" Ingrese su contraseña " 
+          {... register("password", {required:true})}></Input>
+
+          {
+            errors.password && <p className="text-red-500"> Este campo es requerido </p>
+          }
+
+
+
+          <Button>Registrarse</Button>
+        </form>
+      </Card>
+    </div>
+      
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
+
