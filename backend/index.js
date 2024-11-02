b412d9121d95131f3e69447e2ef4fa45833c5da4
Sol Bilyk <solbilyk14@gmail.com>
2024-11-02 01:31:05 -0300

    cambiamos index

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@
 // //     app.listen(3000);
 // //     console.log("Server on port", 3000);
 // //     pool.end();
-// // });
+
 
 // app.listen(3000);  //Esto es para lanzar en el puerto 3000 (CAMBIE EL PUERTO A 3030 PORQUE CON 3000 ME SALIAN ERRORES, DEBIDO A QUE ESE PUERTO ESTA SIENDO USADO TAMBIEN
 // console.log("Server on port", 3000);  //Para lanzarlo escribimos en la terminal npm run dev
