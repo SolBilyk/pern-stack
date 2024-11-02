b781078277c7662282e8a5ac1b3877cd6790c998
Sol Bilyk <solbilyk14@gmail.com>
2024-11-02 01:14:47 -0300

    api/ping

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.json({ message: "Bienvenidos a mi proyecto" }));
-app.get("/api/ping", async (req, res) => {
+app.get("/api/ping", async(req, res) => {
     const result = await pool.query("SELECT NOW()");
     res.json(result.rows[0]);
 });
